da7029080ac3228f43dd3031f2529646ffbe721e
Karthika Hariharan <hariharan.karthika93@gmail.com>
2019-11-01 21:34:46 +0530

    Update queue.js

--- DataStructures/queue.js
+++ DataStructures/queue.js
@@ -1,80 +1,79 @@
-    /** Class representing a Stack. */
-    class Stack {
-
-        constructor() {
+ /** Class representing a Queue. 
+         * @constructor
+        */
+        
+        class Queue {
+        
+          constructor() {
             this._dataStorage = {};
             this._length = 0;
-        }
-
-        /*
-        * Returns the length of the stack
-        */
-        getLength() {
-        return this._length;
-        }
-
-        /*
-        * Adds a new value at the end of the stack
-        * @param {*} value the value to push
-        */
-        push(value) {
+            this._headIndex = 0;
+          }
+          /*
+          * Enqueues a new value at the end of the queue
+          * @param {*} value the value to enqueue
+          */
+          enqueue(value) {
             try{
-            if (!value) {
-                throw "Parameter is not defined";
+             if(!value){
+               throw 'Parameter is not defined'
+             }
+             this._dataStorage[this._headIndex + this._length] = value;
+             this._length++;
             }
-            this._dataStorage[this._length] = value;
-            this._length++;
+            catch(err) {
+              console.log(err);
+            }
+          }
+        
+          /*
+          * Dequeues the value from the beginning of the queue and returns it
+          * @return {*} the first and oldest value in the queue
+          */
+          dequeue() {
+            try{
+              if (!this._length) {
+                throw "The Queue is empty";
+              }
+              var firstVal = this._dataStorage[this._headIndex];
+              this._dataStorage[this._headIndex] = undefined; // can use delete keyword
+              this._length--;
+              this._headIndex++;
+              return firstVal;
             }
             catch(err){
-            console.err(err);
+              console.log(err);
             }
-        }
-
-        /*
-        * Removes the value at the end of the stack and returns it
-        * @return {*} the last and newest value in the stack
-        */
-        pop() {
+          }
+          /*
+          * Returns the value at the beginning of the queue without removing it from the queue
+          * @return {*} the first and oldest value in the queue
+          */
+          peek() {
             try{
-            if (!this._length) {
-                throw "The Stack is empty";
-            }
-            var lastVal = this._dataStorage[this._length - 1];
-            this._dataStorage[this._length - 1] = undefined; // can use delete keyword
-            this._length--;
-            return lastVal;
+              if (!this._length) {
+                throw "The Queue is empty";
+              }
+              return this._dataStorage[this._headIndex];
             }
             catch(err){
-            console.log(err);
+              console.log(err);
             }
+          }
         }
-        /*
-        * Returns the value at the end of the stack without removing it
-        * @return {*} the last and newest value in the stack
-        */
-        peek() {
-            try{
-            if (!this._length) {
-                throw "The Stack is empty";
-            }
-            return this._dataStorage[this._length - 1];
-            }
-            catch(err){
-            console.log(err);
-            }
-        }
-    }
-
-    const myStack = new Stack();
-
-
-    // using stack
-    myStack.push(() => {
-    return 'I am a function written by karthi';
-    });
-    myStack.push('hi');
-    myStack.push('I am karthi');
-
-    console.log(myStack.getLength());
-
-    console.log(myStack)
+        
+        var myQueue = new Queue();
+        myQueue.dequeue();
+        myQueue.enqueue('hi');
+        myQueue.enqueue('bye');
+        myQueue.enqueue('I am back');
+        myQueue.enqueue('I am back2');
+        myQueue.enqueue('I am back3');
+        myQueue.enqueue('bye');
+        myQueue.dequeue();
+        myQueue.dequeue();
+        myQueue.dequeue();
+        
+        
+        console.log(myQueue.peek());
+        `
